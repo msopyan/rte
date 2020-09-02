4e562d5a8392d4d99e860c63a26dfce8b6f61ae7
msopyan <faqihsopyan@gmail.com>
2020-09-02 19:07:22 +0700

    configuration home controllers

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,13 +1,6 @@
 <?php 
 class Home extends CI_Controller{
-	function __construct(){
-		parent::__construct();
-		$this->load->model('m_tulisan');
-		$this->load->model('m_pengunjung');
-        $this->m_pengunjung->count_visitor();
-	}
 	function index(){
-		$x['post']=$this->m_tulisan->get_post_home();
-		$this->load->view('index',$x);
+		$this->load->view('index');
 	}
 }
